76562408cc2cc15e2bfb47b3de32ac24268db8c5
Indrek Rajamets <indrek02@gmail.com>
2013-03-30 10:36:21 +0200

    js fix

--- public/js/common.js
+++ public/js/common.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
 
     $(".datePicker").each(function(){
-        console.log($(this)[0].attributes);
         $(this).datepicker({
             dateFormat: 'dd.mm.yy',
             changeMonth: true,
